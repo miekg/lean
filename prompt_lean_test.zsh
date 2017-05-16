7e7f90f6689dc220f057a3c461a609b19348d6b6
Miek Gieben <miek@miek.nl>
2017-05-16 07:59:54 +0100

    Add tests

--- prompt_lean_test.zsh
+++ prompt_lean_test.zsh
@@ -6,11 +6,12 @@ source prompt_lean_setup
 prompt_lean_precmd
 prompt_lean_preexec
 
-echo -n $PROMPT
-echo -n $RPROMPT
+echo $PROMPT | od
+echo $PROMPT
 
 expect='%F{yellow}%(?.%F{blue}.%B%F{red})%#%f%b\n'
-echo -n $expect
+echo $expect | od
+echo $expect 
 
 if [[ $PROMPT = $expect ]]; then
     print "  OK"
