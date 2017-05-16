80a83149d8035dd0dff997ece5aeeec8558118fb
Miek Gieben <miek@miek.nl>
2017-05-16 09:28:26 +0000

    Add prompt tests

--- Makefile
+++ Makefile
@@ -1,6 +1,3 @@
-all:
-	@echo noop
-
 .PHONY: test
 test:
 	./prompt_lean_test.zsh
--- prompt_lean_test.zsh
+++ prompt_lean_test.zsh
@@ -23,17 +23,59 @@ function comphex() {
 
 source prompt_lean_setup
 
+(
 prompt_lean_precmd
 prompt_lean_preexec
-
-expect='%F{yellow}%(?.%F{blue}.%B%F{red})%#%f%b '
+expect='%F{yellow}%f%f%(?.%F{blue}.%B%F{red})%#%f%b '
 comphex "prompt" $PROMPT $expect
+)
 
 ( cd /tmp
 prompt_lean_precmd
 prompt_lean_preexec
-
-expect='%F{yellow}%f%F{blue}/tmp%F{242}$vcs_info_msg_0_ %F{yellow}%m%f%f'
+expect='%F{yellow}%f%F{blue}/tmp%F{242}$vcs_info_msg_0_ %F{yellow}%m%f%f%f'
 comphex "rprompt" $RPROMPT $expect
+)
+
+(
+function left() { echo left }
+PROMPT_LEAN_LEFT=left
+
+prompt_lean_precmd
+prompt_lean_preexec
+expect='%F{yellow}%fleft%f%(?.%F{blue}.%B%F{red})%#%f%b '
+comphex "lean_left" $PROMPT $expect
+)
+
+( cd /tmp
+function right() { echo right }
+PROMPT_LEAN_RIGHT=right
+
+prompt_lean_precmd
+prompt_lean_preexec
+expect='%F{yellow}%f%F{blue}/tmp%F{242}$vcs_info_msg_0_ %F{yellow}%m%f%fright%f'
+comphex "lean_right" $RPROMPT $expect
+)
+
+(
+PROMPT_LEAN_TMUX='t'
+TMUX='yes'
+source prompt_lean_setup # TMUX is only evaluated in setup
+
+prompt_lean_precmd
+prompt_lean_preexec
+expect='%F{yellow}t%f%f%(?.%F{blue}.%B%F{red})%#%f%b '
+comphex "tmux" $PROMPT $expect
+)
+
+( cd /tmp
+PROMPT_LEAN_CMD_MAX_EXEC_TIME=0.1
+#source prompt_lean_setup # TMUX is only evaluated in setup
+sleep 1
+
+prompt_lean_precmd
+prompt_lean_preexec
+expect='%F{yellow}1s %f%F{blue}/tmp%F{242}$vcs_info_msg_0_ %F{yellow}%m%f%f%f'
+comphex "time" $RPROMPT $expect
 )
 
