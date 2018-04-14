a33b5ae061f4629d80662753e40a3cbede3872f0
Miek Gieben <miek@miek.nl>
2018-04-14 09:18:11 +0100

    New colors

--- prompt_lean_test.zsh
+++ prompt_lean_test.zsh
@@ -3,8 +3,9 @@
 # Some tests for the prompt. We compare the prompt using hex encoding
 # to prevent all the control characters messing up the prompt.
 
-COLOR1="241"
-COLOR2="140"
+COLOR1="242"
+COLOR2="110"
+COLOR3="150"
 
 function comphex() {
     local name="$1"
@@ -30,14 +31,14 @@ source prompt_lean_setup
 (
 prompt_lean_precmd
 prompt_lean_preexec
-expect='%F{yellow}%f%f%(?.%F{'$COLOR2'}.%B%F{red})%#%f%b '
+expect='%F{'$COLOR3'}%f%f%(?.%F{'$COLOR2'}.%B%F{red})%#%f%b '
 comphex "prompt" $PROMPT $expect
 )
 
 ( cd /tmp
 prompt_lean_precmd
 prompt_lean_preexec
-expect='%F{yellow}%f%F{'$COLOR2'}/tmp%F{'$COLOR1'}$vcs_info_msg_0_%f%f'
+expect='%F{'$COLOR3'}%f%F{'$COLOR2'}/tmp%F{'$COLOR1'}$vcs_info_msg_0_%f%f'
 comphex "rprompt" $RPROMPT $expect
 )
 
@@ -47,7 +48,7 @@ PROMPT_LEAN_LEFT=left
 
 prompt_lean_precmd
 prompt_lean_preexec
-expect='%F{yellow}%fleft%f%(?.%F{'$COLOR2'}.%B%F{red})%#%f%b '
+expect='%F{'$COLOR3'}%fleft%f%(?.%F{'$COLOR2'}.%B%F{red})%#%f%b '
 comphex "lean_left" $PROMPT $expect
 )
 
@@ -57,7 +58,7 @@ PROMPT_LEAN_RIGHT=right
 
 prompt_lean_precmd
 prompt_lean_preexec
-expect='%F{yellow}%f%F{'$COLOR2'}/tmp%F{'$COLOR1'}$vcs_info_msg_0_%fright%f'
+expect='%F{'$COLOR3'}%f%F{'$COLOR2'}/tmp%F{'$COLOR1'}$vcs_info_msg_0_%fright%f'
 comphex "lean_right" $RPROMPT $expect
 )
 
@@ -68,7 +69,7 @@ source prompt_lean_setup # TMUX is only evaluated in setup
 
 prompt_lean_precmd
 prompt_lean_preexec
-expect='%F{yellow}t%f%f%(?.%F{'$COLOR2'}.%B%F{red})%#%f%b '
+expect='%F{'$COLOR3'}t%f%f%(?.%F{'$COLOR2'}.%B%F{red})%#%f%b '
 comphex "tmux" $PROMPT $expect
 )
 
@@ -78,6 +79,6 @@ sleep 1
 
 prompt_lean_precmd
 prompt_lean_preexec
-expect='%F{yellow}1s %f%F{'$COLOR2'}/tmp%F{'$COLOR1'}$vcs_info_msg_0_%f%f'
+expect='%F{'$COLOR3'}1s %f%F{'$COLOR2'}/tmp%F{'$COLOR1'}$vcs_info_msg_0_%f%f'
 comphex "time" $RPROMPT $expect
 )
